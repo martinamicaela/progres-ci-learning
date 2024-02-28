4c9c82b0f6de746d711f485767a462bcd172fd22
martinamicaela frutos <martinamicaela924@gmail.com>
2024-02-28 22:11:44 +0000

    add database chinook to connect()

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -1,3 +1,21 @@
 import psycopg2
 # conect to "chinook" database
-connection= psycopg2.conect(database="Chinook")
+connection= psycopg2.connect(database="chinook")
+
+
+# build a cursor object of the database 
+cursor= connection.cursor()
+# Query 1 select all records from "artist" table 
+cursor.execute('SELECT * FROM "artist"')
+
+#fetch the results (multiple)
+results=cursor.fetchall()
+#fetch the results (single)
+#results=connection.fetchone()
+
+#close the connection 
+connection.close()
+
+# print results 
+for result in results:
+    print(result)
