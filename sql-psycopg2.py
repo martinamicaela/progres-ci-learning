1c3558dc476e532ab1cbe16d5ffce81c849fecba
martinamicaela frutos <martinamicaela924@gmail.com>
2024-02-29 21:52:45 +0000

    S

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -5,13 +5,29 @@ connection= psycopg2.connect(database="chinook")
 
 # build a cursor object of the database 
 cursor= connection.cursor()
-# Query 1 select all records from "artist" table 
-cursor.execute('SELECT * FROM "artist"')
+# Query 1- select all records from "artist" table 
+#cursor.execute('SELECT * FROM "artist"')
+
+#Query 2- select only the "Name" column from the "Artist" table 
+#cursor.execute('SELECT "name" FROM "artist"')
+
+#Query 3- select only the "Name" column from the "Artist" table 
+#cursor.execute('SELECT * FROM "artist" WHERE "name"= %s', ["Queen"])
+
+#Query 4- select only by the "artistid" #51 from "artist" table
+#cursor.execute('SELECT * FROM "artist" WHERE "artist_id"= %s', [51])
+
+#Query 5- select only the albums with "artist_id" #51 on the "album" table 
+#cursor.execute('SELECT * FROM "album" WHERE "artist_id" = %s',[51])
+
+#Query 5- select all tracks where the composer is "Queen" from the "track" table
+cursor.execute('SELECT * FROM "track" WHERE "composer"= %s', ["Queen"])
 
 #fetch the results (multiple)
 results=cursor.fetchall()
+
 #fetch the results (single)
-#results=connection.fetchone()
+#results=cursor.fetchone()
 
 #close the connection 
 connection.close()
